57f333570d6fe980e08a98c878568c1c50c7e611
Trixia Du <trixiadu50@gmail.com>
2023-10-14 22:44:25 +0800

    Updated movie design

--- BSIT3L-Movies/Models/MoviesModel.cs
+++ BSIT3L-Movies/Models/MoviesModel.cs
@@ -9,7 +9,8 @@ namespace BSIT3L_Movies.Models
         public string Rating { get; set; }
         public int ReleaseYear { get; set; }
         public string Genre { get; set; }
-        
+        public string Description { get; set; }
+
     }
 }
 
